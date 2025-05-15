293f7cad2a43d322002bb191beedac0482cc94ef
benardelia <agustinbenard2000@gmail.com>
2025-05-15 19:00:33 +0300

    neow

--- src/routes/axiosAuthInstatnce.js
+++ src/routes/axiosAuthInstatnce.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const session = localStorage.getItem("jwt_token");
                  const axiosAuthInstance = axios.create({
-                             baseURL: "http://82.25.119.147:1966",
+                             baseURL: "/api",
                                  timeout: 9000,
                                  headers: {
                                      "Content-Type": "application/json"
--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,5 @@ export default defineConfig({
       
     },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      
-    }
-  },
   assetsInclude: "**/*.JPG"
 });
