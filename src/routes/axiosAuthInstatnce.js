803c77a7208153c5919b47de908035d3eed425e0
tech-nassir <gmasuke29@example.com>
2025-08-02 18:10:45 +0300

    jackgolds

--- src/routes/axiosAuthInstatnce.js
+++ src/routes/axiosAuthInstatnce.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
                            
                  const axiosAuthInstance = axios.create({
-                             baseURL: "https://grandypos.duckkdns.org",
+                             baseURL: "https://grandypos.duckdns.org",
                                  timeout: 9000,
                                  headers: {
                                      "Content-Type": "application/json"
